aebed9cb0f581fad9672dc58de47bf408a76a0f6
mrdzsi <jenofigergo@gmail.com>
2022-11-09 20:43:15 +0100

    Added: basic browsing

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
-
     return render_template("index.html")
+
+
+@app.route('/browse')
+def browse():
+    return render_template("browse.html")
+
+
+@app.route('/swipe')
+def swipe():
+    return render_template("swipe.html")
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
 
 @app.route('/user/<name>')
 def user(name):
