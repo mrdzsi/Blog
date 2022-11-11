cb23a7c0b20086c80dbd0601040cef51df39cc5c
mrdzsi <jenofigergo@gmail.com>
2022-11-11 12:46:42 +0100

    Added: basic bootstrap

--- app.py
+++ app.py
@@ -24,12 +24,6 @@ def about():
     return render_template("about.html")
 
 
-@app.route('/user/<name>')
-def user(name):
-    return render_template("user.html",
-                            user_name=name)
-
-
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("404.html"), 404
